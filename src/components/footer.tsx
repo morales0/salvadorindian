4fefff74a434da8a2f4e88a210b29e58ed14605d
Francisco Morales <chico.fmorales@gmail.com>
2022-11-05 22:59:33 -0700

    updating footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,6 @@ type FooterProps = {
 const Footer = (props: FooterProps) => {
   return (
     <div className="flex w-full px-4 py-4 bg-slate-200">
-      <p className="text-sm text-slate-800 font-semibold italic">Moraliam</p>
       <p className="ml-auto text-xs text-gray-800">Made by Carolyn and Francisco</p>
     </div>
   )
