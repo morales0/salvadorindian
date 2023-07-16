05180c863fa0a3c8074d4cdbe3d2b8bd1820523b
Francisco Morales <chico.fmorales@gmail.com>
2023-07-16 14:43:13 -0700

    Adding in recipes list

--- src/pages/recipes/[slug].tsx
+++ src/pages/recipes/[slug].tsx
@@ -3,11 +3,27 @@ import { getAllRecipesWithSlug, getRecipe } from "data/contentful";
 import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
 
 const Recipe = ({ recipe }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  console.log(recipe);
+  const ingredients = recipe?.fields?.ingredients;
+
   return (
-    <div>
+    <div className="p-4">
       <h1>{recipe?.fields?.name}</h1>
       <div>{documentToReactComponents(recipe?.fields?.description)}</div>
-      <h2>Procedure</h2>
+      <h2 className="font-bold text-lg">Ingredients</h2>
+      <div>
+        {ingredients?.map(
+          ({ sys: { id }, fields: { name, value, unit, annotation } }) => (
+            <div key={id}>
+              <span className="font-bold">
+                {value} {unit}
+              </span>
+              , {name}, <span className="italic">{annotation}</span>
+            </div>
+          )
+        )}
+      </div>
+      <h2 className="font-bold text-lg">Procedure</h2>
       <div>{documentToReactComponents(recipe?.fields?.procedure)}</div>
     </div>
   );
--- src/pages/recipes/index.tsx
+++ src/pages/recipes/index.tsx
@@ -1,7 +1,36 @@
+import { GetStaticProps } from "next";
 import Layout from "../../components/ui/layout/layout";
+import { getAllRecipesWithSlug } from "data/contentful";
+import Link from "next/link";
 
-const Recipes = () => {
-  return <div>Recipes</div>;
+const Recipes = ({ recipesData }) => {
+  return (
+    <div className="p-4">
+      {recipesData.map((r) => (
+        <Link
+          key={r.slug}
+          href={`recipes/${r.slug}`}
+          className="underline text-amber-600"
+        >
+          {r.name}
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allRecipes = await getAllRecipesWithSlug();
+  const recipesData = allRecipes.items.map((item) => ({
+    name: item.fields.name,
+    slug: item.fields.slug,
+  }));
+
+  return {
+    props: {
+      recipesData,
+    },
+  };
 };
 
 export default Recipes;
