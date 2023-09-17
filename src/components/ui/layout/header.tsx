0b214a615f6b6cee4e0108db37d9f3b11ad35cdc
Francisco Morales <chico.fmorales@gmail.com>
2023-09-16 19:47:18 -0700

    Added responsive header with search

--- src/components/ui/layout/header.tsx
+++ src/components/ui/layout/header.tsx
@@ -4,72 +4,86 @@ import { Icon } from "@iconify/react";
 import styles from "./header.module.scss";
 import { NavItem } from "./nav-item";
 import clsx from "clsx";
-import salvadorindianBrand from "@assets/salvadorindian-brand-logo.svg";
+import salvadorindianLogo from "@assets/salvadorindian-logo.svg";
 import Image from "next/image";
 import { useRouter } from "next/router";
 
 // Data
 const mainLinks = [
-  ["Blog", "/blog"],
-  ["Recipes", "/recipes"],
-  ["Inspo", "/inspo"],
-  ["Favorites", "/favorites"],
+  ["blog", "/blog"],
+  ["original recipes", "/recipes"],
+  ["inspiration", "/inspo"],
 ];
 const secondaryLinks = [
-  ["About", "/about"],
-  ["Contact", "/contact"],
+  ["home", "/"],
+  ["about", "/about"],
+  ["contact", "/contact"],
 ];
-
-const Brand = () => (
-  <div className="flex flex-row">
-    <div className={clsx(styles.brand)}>Salvador</div>
-    <div>Indian</div>
-  </div>
-);
 
 // Component
 export const Header = () => {
   const router = useRouter();
-  const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
     <div>
       <div className="px-4 py-2 bg-slate-200">
-        <div className="flex flex-row">
-          <nav className="flex flex-row gap-4">
-            <Link href="/">home</Link>
-            <div>about</div>
-            <div>contact</div>
+        <div className="flex flex-row justify-center">
+          <nav className="flex flex-row gap-4 text-slate-600">
+            {secondaryLinks.map(([name, href]) => (
+              <Link key={name} href={href}>
+                {name}
+              </Link>
+            ))}
           </nav>
         </div>
       </div>
-      <div className="flex flex-row items-center py-6">
-        <Image src={salvadorindianBrand} alt="Brand" height={140} />
-        <div className="flex flex-col items-start gap-3 text-2xl">
-          <Link
-            className={clsx(styles.navLink, {
-              [styles.navLinkActive]: router.pathname === "/recipes",
-            })}
-            href="/recipes"
+
+      <nav className="flex flex-col items-center py-3 px-6 gap-3 sm:flex-row sm:justify-center sm:gap-6 md:gap-8">
+        <Link href="/" className="sm:py-4">
+          <Image src={salvadorindianLogo} alt="Brand" height={125} />
+        </Link>
+        <div className="flex flex-row gap-4 sm:flex-col items-start  text-base sm:text-lg md:text-xl lg:text-2xl">
+          {mainLinks.map(([name, href]) => (
+            <Link
+              key={name}
+              className={clsx(styles.navLink, {
+                [styles.navLinkActive]: router.pathname === href,
+              })}
+              href={href}
+            >
+              {name}
+            </Link>
+          ))}
+        </div>
+      </nav>
+
+      <div className="container px-8 m-auto flex flex-row justify-center max-w-xl">
+        <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+          <input
+            type="search"
+            className="relative m-0 block w-[1px] min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[0_0_0_1px_rgb(59,113,202)] focus:outline-none"
+            placeholder="Search"
+            aria-label="Search"
+            aria-describedby="button-addon2"
+          />
+
+          <span
+            className="input-group-text flex items-center whitespace-nowrap rounded px-3 py-1.5 text-center text-base font-normal text-neutral-700"
+            id="basic-addon2"
           >
-            original recipes
-          </Link>
-          <Link
-            className={clsx(styles.navLink, {
-              [styles.navLinkActive]: router.pathname === "/blog",
-            })}
-            href="/blog"
-          >
-            blog
-          </Link>
-          <Link
-            className={clsx(styles.navLink, {
-              [styles.navLinkActive]: router.pathname === "/inspo",
-            })}
-            href="/inspo"
-          >
-            inspiration
-          </Link>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </span>
         </div>
       </div>
     </div>
